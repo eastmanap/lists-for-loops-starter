aca612c2a885ce0c95741f0b6b884e713a0bfa5b
eastmanap <eastmanap354@northwested.org>
2024-11-06 10:44:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Apollos Eastman
+# Nov 6 20024
+# lists For Loops ver3
+
+import time
+# 4-1 Pizzas
+fav_pizzas = ['Deep Dish', 'French Bread', 'Hawaiian', 'Meat Lover\'s',]
+
+for pizza in fav_pizzas:
+    print(f'\n{pizza} is one of my favorite pizzas!')
+    time.sleep(.5)
+
+print('\nI really love pizza!')
+
+# 4-2 Animals
+animals = ['Orangutan','Lemur','Sugar Glider','Spider Monkey']
+
+for animal in animals:
+    print(f'\n {animal} are super cool and cute!')
+    time.sleep(.5)
+
+print('\nAll of these animals have opposable thumbs!')
